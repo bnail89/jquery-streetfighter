a96d4bb80ad8866a5d91d0fb75b4cd366f2fb6ba
bnail89 <bnail89@gmail.com>
2016-07-20 03:34:19 -0500

    completed project

--- js/app.js
+++ js/app.js
@@ -39,10 +39,58 @@ $('body').on('keydown', function(e) {
         }
     });
 
+$(document).ready(function(){
+    $('.hulk-ryu').mouseenter(function(){
+        $('.hulk-ryu-still').hide();
+        $('.hulk-ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.hulk-ryu-still').show();
+        $('.hulk-ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHulkHadouken();
+        $('.hulk-ryu-ready').hide();
+        $('.hulk-ryu-throwing').show();
+        $('.hulk-hadouken').finish().show().animate(
+            {'right': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('right', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.hulk-ryu-throwing').hide();
+        $('.hulk-ryu-ready').show();
+    });
+});
+
+$('body').on('keydown', function(e) {
+        if (e.which == 90) {
+            $('.hulk-ryu-still').hide();
+            $('.hulk-ryu-ready').hide();
+            $('.hulk-ryu-cool').show();
+        }
+    })
+       .on('keyup', function(e) {
+        if (e.which == 90) {
+            $('.hulk-ryu-cool').hide();
+            $('.hulk-ryu-still').show();
+        }
+    });
+
 function playHadouken() {
     $('#hadouken-sound')[0].volume = 0.5;
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
+}
+
+function playHulkHadouken() {
+    $('#hulk-hadouken-sound')[0].volume = 0.5;
+     $('#hulk-hadouken-sound')[0].setrate = -1;
+    $('#hulk-hadouken-sound')[0].load();
+    $('#hulk-hadouken-sound')[0].play();
 }
 
 
