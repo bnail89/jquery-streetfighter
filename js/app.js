026e8647eefbb33dc04fefbd7d4a7dfe2977bae9
bnail89 <bnail89@gmail.com>
2016-07-20 01:18:48 -0500

    Keydown function added

--- js/app.js
+++ js/app.js
@@ -23,11 +23,27 @@ $(document).ready(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
     });
-
 });
+
+$('body').on('keydown', function(e) {
+        if (e.which == 88) {
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-cool').show();
+        }
+    })
+       .on('keyup', function(e) {
+        if (e.which == 88) {
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        }
+    });
 
 function playHadouken() {
     $('#hadouken-sound')[0].volume = 0.5;
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
 }
+
+
+
